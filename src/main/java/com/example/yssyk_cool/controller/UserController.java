f94864192ddc58acb0eb4786c4444371e4acb203
raatbek <jumabekovradik@gmail.com>
2022-07-04 20:02:47 +0600

    second commit

--- src/main/java/com/example/yssyk_cool/controller/UserController.java
+++ src/main/java/com/example/yssyk_cool/controller/UserController.java
@@ -31,6 +31,7 @@ public class UserController {
         return userService.save(request);
     }
 
+
     @PostMapping("/auto")
     public UserTokenResponse auto(@RequestBody UserAuthRequest request) {
         return userService.getToken(request);
